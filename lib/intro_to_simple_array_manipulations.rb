7eae70609795c4f6e5ab0080dbd3619bc0a9524f
Reynaldo Ayala <reynaldo-ayala@hotmail.com>
2020-05-27 21:06:52 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_concat(argument1, argument2)
+  argument1.concat(argument2)
+end
+
+def using_insert(argument1, argument2)
+  argument1.insert(4, "Python")
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, name)
+  array.delete(name)
+end
+
+def using_delete_at(array, x)
+  array.delete_at(x)
+end
+
+
+
+  
