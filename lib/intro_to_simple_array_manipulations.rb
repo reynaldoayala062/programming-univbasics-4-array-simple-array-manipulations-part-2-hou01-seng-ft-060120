27a88e4ad3dbb97425a6b7a40dec4f7a935eee10
Reynaldo Ayala <reynaldo-ayala@hotmail.com>
2020-05-27 20:58:44 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,12 @@
+def using_concat(argument1, argument2)
+  argument1.concat(argument2)
+end
+
+def using_insert(argument1, argument2)
+  argument1.insert(4, "Python")
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
